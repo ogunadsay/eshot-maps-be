720262e52c0b0a689ad97a1134a00be3f130b840
Ogun Adsay <ogun.adsay@emakina.com.tr>
2022-05-29 15:32:51 +0300

    better controller structure
    getall method added

--- controllers/busLine.controller.js
+++ controllers/busLine.controller.js
@@ -2,8 +2,18 @@ const models = require('../db/models')
 
 const BusLine = models.BusLine;
 
-exports.findById = (lineId) => {
-  return BusLine.findByPk(lineId, {include: ["busLineDetails"]})
-    .then(busLine => busLine)
-    .catch((err) => console.log(">> Error while finding busline: ", err));
+exports.findById = async (req, res) => {
+  const lineId = req.params.lineId
+  const busLine = await BusLine.findByPk(lineId, {include: ["busLineDetails"]});
+
+  if(!busLine) return res.status(404).send('Bus line not found')
+
+  res.send(busLine)
 };
+
+exports.getAll = async (req, res) => {
+  const allBusLines = await BusLine.findAll()
+  if(!allBusLines || allBusLines.length === 0) return res.status(404).send('There are no bus lines')
+
+  res.send(allBusLines)
+}
--- routes/busLine.router.js
+++ routes/busLine.router.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const controller = require('../controllers/busLine.controller')
+const {findById, getAll} = require('../controllers/busLine.controller')
 
 router.get('/', function (req, res, next) {
   res.send('API is working properly');
 });
 
-router.get('/:lineId', async (req, res) => {
-  let all = await controller.findById(req.params.lineId);
-  res.send(all)
-})
+router.get('/getAll', getAll)
+router.get('/:lineId', findById)
 
 module.exports = router;
